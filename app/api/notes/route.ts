4c3b75be56a5a39ddaf9eec00eda4f47209a85b7
azacdev <azacdev@gmail.com>
2024-04-07 21:15:14 +0100

    feat: zod validator

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -1,6 +1,25 @@
+import prisma from "@/lib/db";
+import { createNoteSchema } from "@/schemas";
+import { auth } from "@clerk/nextjs";
+
 export async function POST(req: Request) {
   try {
     const body = await req.json();
+    const parseResult = createNoteSchema.safeParse(body);
+
+    if (!parseResult.success) {
+      console.error(parseResult.error);
+      return Response.json({ error: "Invalid input" }, { status: 400 });
+    }
+
+    const { title, content } = parseResult.data;
+    const { userId } = auth();
+
+    if (!userId) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    // const note = await 
   } catch (error) {
     console.log(error);
     return Response.json({ error: "Internal server error" }, { status: 500 });
